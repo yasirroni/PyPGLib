3a904172536bcb7067022ddb04bfc98c997ae16b
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2023-04-03 18:57:25 +0700

    fix packace extra_files

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
+import os
 from setuptools import setup
 
-setup()
+
+def package_files(directory_list):
+    paths = []
+    for directory in directory_list:
+        for (path, _, filenames) in os.walk(directory):
+            for filename in filenames:
+                paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+extra_files = package_files([
+    'pypglib/hvdc',
+    'pypglib/opf',
+    'pypglib/uc',
+])
+
+setup(
+    package_data={
+        '': extra_files,
+    }
+)
