089b8ed7bed82c5d8b6434aaeb92a6643a6be0fc
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2023-04-03 16:48:05 +0700

    add pre-commit and fix workdlow

--- pypglib/__init__.py
+++ pypglib/__init__.py
@@ -2,6 +2,7 @@ import os
 
 PATH_PYPGLIB = os.path.dirname(os.path.abspath(__file__))
 
+
 def __getattr__(filename):
     """Automatically creates a string to case path."""
 
@@ -9,7 +10,7 @@ def __getattr__(filename):
     root, ext = os.path.splitext(filename)
     filename = root + '.m'
 
-    for root, dirs, files in os.walk(PATH_PYPGLIB):
+    for root, _, files in os.walk(PATH_PYPGLIB):
         for name in files:
             if name == filename:
                 return os.path.join(root, name)
--- tests/test_import.py
+++ tests/test_import.py
@@ -4,11 +4,11 @@ import pytest
 
 import pypglib
 
-
 '''
 pytest -rA -c pyproject.toml --cov-report term-missing --cov=pypglib
 pytest -n auto -rA -c pyproject.toml --cov-report term-missing --cov=pypglib
 '''
+
 
 def test_import_contains_data():
     pglib_path = os.path.abspath(os.path.dirname(pypglib.__file__))
@@ -17,24 +17,27 @@ def test_import_contains_data():
     assert os.path.isdir(os.path.join(pglib_path, "uc"))
     assert os.path.isdir(os.path.join(pglib_path, "hvdc"))
 
+
 def test_import_case():
-    from pypglib import (pglib_opf_case14_ieee,
-                         pglib_opf_case14_ieee__sad,
-                         pglib_opf_case14_ieee__api,
-                         case5_3_he,
-                        )
+    from pypglib import (
+        case5_3_he,
+        pglib_opf_case14_ieee,
+        pglib_opf_case14_ieee__api,
+        pglib_opf_case14_ieee__sad,
+    )
     assert pglib_opf_case14_ieee.endswith("pglib_opf_case14_ieee.m")
     assert pglib_opf_case14_ieee__sad.endswith("pglib_opf_case14_ieee__sad.m")
     assert pglib_opf_case14_ieee__api.endswith("pglib_opf_case14_ieee__api.m")
     assert case5_3_he.endswith("case5_3_he.m")
 
     with pytest.raises(FileNotFoundError):
-        from pypglib import case9
+        from pypglib import case9  # noqa F401
+
 
 def test_get_case():
     # opf
     assert pypglib.pglib_opf_case14_ieee.endswith("pglib_opf_case14_ieee.m")
-    
+
     # opf-sad
     assert pypglib.pglib_opf_case14_ieee__sad.endswith("pglib_opf_case14_ieee__sad.m")
 
